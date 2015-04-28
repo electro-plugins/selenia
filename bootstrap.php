eb357cf158b259fb804464a8911f237e67a046e9
Cláudio Silva <claudio.silva@impactwave.com>
2015-04-28 18:51:21 +0100

    Default config settings.

--- bootstrap.php
+++ bootstrap.php
@@ -6,4 +6,9 @@ ModuleOptions (__DIR__, [
   'less'      => 'main.less',
   'templates' => true,
   'views'     => true,
+  'config'    => [
+    'main' => [
+      'userModel' => 'Selene\Modules\Admin\Models\User',
+    ]
+  ]
 ]);
