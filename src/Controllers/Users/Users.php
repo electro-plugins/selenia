de764872f53fd8a837c5f5f8f0a4aa2d5c84a47b
Cláudio Silva <claudio.silva@impactwave.com>
2015-09-11 17:29:55 +0100

    FIX: 403 error

--- src/Controllers/Users/Users.php
+++ src/Controllers/Users/Users.php
@@ -6,14 +6,6 @@ use Selenia\Plugins\AdminInterface\Controllers\AdminController;
 
 class Users extends AdminController
 {
-  protected function initialize ()
-  {
-    global $session;
-    if (!$session->user)
-      throw new HttpException(403);
-    parent::initialize ();
-  }
-
   public function interceptViewDataSet ($dataSourceName, array &$data)
   {
     $data = $this->dataItem->map ($data, function (UserInterface $user) {
