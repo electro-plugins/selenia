7a26ede7e13dbff7c894c1613003b2ef29aa62a6
Cláudio Silva <claudio.silva@impactwave.com>
2016-04-07 03:44:11 +0100

    FIX: migrations. FIX: databinding syntax

--- database/migrations/20160229113413_admin_tables.php
+++ database/migrations/20160229113413_admin_tables.php
@@ -27,48 +27,52 @@ class AdminTables extends Migration
   function up ()
   {
     $schema = $this->db->schema ();
+    $this->db->connection ()->transaction (function () use ($schema) {
 
-    if (!$schema->hasTable ('users')) {
-      $schema->create ('users', function (Blueprint $t) {
-        $t->increments ('id');
-        $t->timestamps ();
-        $t->timestamp ('lastLogin')->nullable ();
-        $t->string ('username', 30)->unique ();
-        $t->string ('password', 60);
-        $t->string ('realName', 30);
-        $t->tinyInteger ('role');
-        $t->boolean ('active')->default (false);
-        $t->rememberToken ();
-      });
-      $now = Carbon::now ();
-      $this->db->table ('users')->insert ([
-        'username'   => 'admin',
-        'role'       => UserInterface::USER_ROLE_ADMIN,
-        'created_at' => $now,
-        'updated_at' => $now,
-        'active'     => true,
-      ]);
-    }
-    else $this->output->writeln (" == Table <info>users</info> already exists. Skipped.");
+      if (!$schema->hasTable ('users')) {
+        $schema->create ('users', function (Blueprint $t) {
+          $t->increments ('id');
+          $t->timestamps ();
+          $t->timestamp ('lastLogin')->nullable ();
+          $t->string ('username', 30)->unique ();
+          $t->string ('password', 60);
+          $t->string ('realName', 30);
+          $t->tinyInteger ('role');
+          $t->boolean ('active')->default (false);
+          $t->rememberToken ();
+        });
+        $now = Carbon::now ();
+        $this->db->table ('users')->insert ([
+          'username'   => 'admin',
+          'password'   => '',
+          'realName'   => 'Admin',
+          'role'       => UserInterface::USER_ROLE_ADMIN,
+          'created_at' => $now,
+          'updated_at' => $now,
+          'active'     => true,
+        ]);
+      }
+      else $this->output->writeln (" == Table <info>users</info> already exists. Skipped.");
 
-    if (!$schema->hasTable ('files'))
-      $schema->create ('files', function (Blueprint $t) {
-        $t->uuid ('id');
-        $t->timestamps ();
-        $t->string ('name', 64);
-        $t->string ('ext', 4);
-        $t->string ('mime', 129);
-        $t->morphs ('owner');
-        $t->string ('group', 16)->nullable ();
-        $t->boolean ('image')->default (false);
-        $t->string ('path', 40);
-        $t->string ('metadata', 1024)->nullable ();
+      if (!$schema->hasTable ('files'))
+        $schema->create ('files', function (Blueprint $t) {
+          $t->uuid ('id');
+          $t->timestamps ();
+          $t->string ('name', 64);
+          $t->string ('ext', 4);
+          $t->string ('mime', 129);
+          $t->morphs ('owner');
+          $t->string ('group', 16)->nullable ();
+          $t->boolean ('image')->default (false);
+          $t->string ('path', 40);
+          $t->string ('metadata', 1024)->nullable ();
 
-        $t->primary ('id');
-        $t->index ('owner_type');
-        $t->index ('image');
-      });
+          $t->primary ('id');
+          $t->index ('owner_type');
+          $t->index ('image');
+        });
 
+    });
   }
 
 }
