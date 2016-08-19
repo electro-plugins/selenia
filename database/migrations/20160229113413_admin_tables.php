5d21daaad4cc69bbac6691545a70fc8abce0cc27
Cláudio Silva <claudio.silva@impactwave.com>
2016-08-19 02:49:44 +0100

    FIX: migrations.

--- database/migrations/20160229113413_admin_tables.php
+++ database/migrations/20160229113413_admin_tables.php
@@ -1,8 +1,8 @@
 <?php
 use Carbon\Carbon;
-use Illuminate\Database\Schema\Blueprint;
 use Electro\Interfaces\UserInterface;
 use Electro\Plugins\IlluminateDatabase\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class AdminTables extends Migration
 {
@@ -19,11 +19,13 @@ class AdminTables extends Migration
       $schema->drop ('files');
       $this->output->writeln ("  Dropped table <info>files</info>.");
     }
+    else $this->output->writeln (" == Table <info>files</info> doesn't exist. Skipped.");
 
     if ($schema->hasTable ('users')) {
       $schema->drop ('users');
       $this->output->writeln ("  Dropped table <info>users</info>.");
     }
+    else $this->output->writeln (" == Table <info>users</info> doesn't exist. Skipped.");
   }
 
   /**
@@ -34,51 +36,49 @@ class AdminTables extends Migration
   function up ()
   {
     $schema = $this->db->schema ();
-    $this->db->connection ()->transaction (function () use ($schema) {
 
-      if (!$schema->hasTable ('users')) {
-        $schema->create ('users', function (Blueprint $t) {
-          $t->increments ('id');
-          $t->timestamps ();
-          $t->timestamp ('lastLogin')->nullable ();
-          $t->string ('username', 30)->unique ();
-          $t->string ('password', 60);
-          $t->string ('realName', 30);
-          $t->tinyInteger ('role');
-          $t->boolean ('active')->default (false);
-          $t->rememberToken ();
-        });
-        $now = Carbon::now ();
-        $this->db->table ('users')->insert ([
-          'username'   => 'admin',
-          'password'   => '',
-          'realName'   => 'Admin',
-          'role'       => UserInterface::USER_ROLE_ADMIN,
-          'created_at' => $now,
-          'updated_at' => $now,
-          'active'     => true,
-        ]);
-      }
-      else $this->output->writeln (" == Table <info>users</info> already exists. Skipped.");
+    if (!$schema->hasTable ('users')) {
+      $schema->create ('users', function (Blueprint $t) {
+        $t->increments ('id');
+        $t->timestamps ();
+        $t->timestamp ('lastLogin')->nullable ();
+        $t->string ('username', 30)->unique ();
+        $t->string ('password', 60);
+        $t->string ('realName', 30);
+        $t->tinyInteger ('role');
+        $t->boolean ('active')->default (false);
+        $t->rememberToken ();
+      });
+      $now = Carbon::now ();
+      $this->db->table ('users')->insert ([
+        'username'   => 'admin',
+        'password'   => '',
+        'realName'   => 'Admin',
+        'role'       => UserInterface::USER_ROLE_ADMIN,
+        'created_at' => $now,
+        'updated_at' => $now,
+        'active'     => true,
+      ]);
+    }
+    else $this->output->writeln (" == Table <info>users</info> already exists. Skipped.");
 
-      if (!$schema->hasTable ('files'))
-        $schema->create ('files', function (Blueprint $t) {
-          $t->uuid ('id');
-          $t->timestamps ();
-          $t->string ('name', 64);
-          $t->string ('ext', 4);
-          $t->string ('mime', 129);
-          $t->morphs ('owner');
-          $t->string ('group', 16)->nullable ();
-          $t->boolean ('image')->default (false);
-          $t->string ('path', 40);
-          $t->string ('metadata', 1024)->nullable ();
+    if (!$schema->hasTable ('files'))
+      $schema->create ('files', function (Blueprint $t) {
+        $t->uuid ('id');
+        $t->timestamps ();
+        $t->string ('name', 64);
+        $t->string ('ext', 4);
+        $t->string ('mime', 129);
+        $t->morphs ('owner');
+        $t->string ('group', 16)->nullable ();
+        $t->boolean ('image')->default (false);
+        $t->string ('path', 40);
+        $t->string ('metadata', 1024)->nullable ();
 
-          $t->primary ('id');
-          $t->index ('image');
-        });
+        $t->primary ('id');
+        $t->index ('image');
+      });
 
-    });
   }
 
 }
