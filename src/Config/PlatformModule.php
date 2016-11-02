6ef32471a7d53c0d46af8adbe7de4e80e1f23349
Cláudio Silva <claudio.silva@impactwave.com>
2016-11-02 18:44:35 +0000

    FIX: bootstrapping upgrade.

--- src/Config/PlatformModule.php
+++ src/Config/PlatformModule.php
@@ -5,6 +5,7 @@ use Electro\Application;
 use Electro\Authentication\Config\AuthenticationSettings;
 use Electro\Core\Assembly\ModuleInfo;
 use Electro\Core\Assembly\Services\Bootstrapper;
+use Electro\Core\Profiles\WebProfile;
 use Electro\Interfaces\DI\InjectorInterface;
 use Electro\Interfaces\Http\Shared\ApplicationMiddlewareInterface;
 use Electro\Interfaces\Http\Shared\ApplicationRouterInterface;
@@ -28,42 +29,43 @@ class PlatformModule implements ModuleInterface
 
   static function bootUp (Bootstrapper $bootstrapper, ModuleInfo $moduleInfo)
   {
-    $bootstrapper
-      //
-      ->on (REGISTER_SERVICES,
-        function (InjectorInterface $injector) {
-          $injector
-            ->share (PlatformSettings::class);
-        })
-      //
-      ->on (CONFIGURE,
-        function (MatisseSettings $matisseSettings, AuthenticationSettings $authSettings, Application $app,
-                  ApplicationMiddlewareInterface $middleware, LocalizationSettings $localizationSettings,
-                  NavigationSettings $navigationSettings, ViewEngineSettings $viewEngineSettings)
-        use ($moduleInfo) {
-          $localizationSettings->registerTranslations ($moduleInfo);
-          $navigationSettings->registerNavigation (Navigation::class);
-          $authSettings->userModel (UserModel::class);
-          $viewEngineSettings->registerViews ($moduleInfo);
-          $matisseSettings
-            ->registerMacros ($moduleInfo)
-            ->registerPresets ([Config\PlatformPresets::class])
-            ->registerComponents ([
-              'LanguageSelector' => LanguageSelector::class,
-            ])
-            // DO NOT IMPORT THE FOLLOWING NAMESPACE!
-            ->registerControllersNamespace ($moduleInfo, \Selenia\Platform\Components::class, 'platform');
+    if ($bootstrapper->profile instanceof WebProfile)
+      $bootstrapper
+        //
+        ->on (REGISTER_SERVICES,
+          function (InjectorInterface $injector) {
+            $injector
+              ->share (PlatformSettings::class);
+          })
+        //
+        ->on (CONFIGURE,
+          function (MatisseSettings $matisseSettings, AuthenticationSettings $authSettings, Application $app,
+                    ApplicationMiddlewareInterface $middleware, LocalizationSettings $localizationSettings,
+                    NavigationSettings $navigationSettings, ViewEngineSettings $viewEngineSettings)
+          use ($moduleInfo) {
+            $localizationSettings->registerTranslations ($moduleInfo);
+            $navigationSettings->registerNavigation (Navigation::class);
+            $authSettings->userModel (UserModel::class);
+            $viewEngineSettings->registerViews ($moduleInfo);
+            $matisseSettings
+              ->registerMacros ($moduleInfo)
+              ->registerPresets ([Config\PlatformPresets::class])
+              ->registerComponents ([
+                'LanguageSelector' => LanguageSelector::class,
+              ])
+              // DO NOT IMPORT THE FOLLOWING NAMESPACE!
+              ->registerControllersNamespace ($moduleInfo, \Selenia\Platform\Components::class, 'platform');
 
-          if ($app->isWebBased) {
-            $middleware->add (AutoRoutingMiddleware::class, null, null, 'router');
-            //$middleware->add (route ('admin/', page ('platform/home.html')), null, 'notFound');
-          };
-        })
-      //
-      ->on (RECONFIGURE,
-        function (ApplicationRouterInterface $router) {
-          $router->add (Routes::class, 'platform');
-        });
+            if ($app->isWebBased) {
+              $middleware->add (AutoRoutingMiddleware::class, null, null, 'router');
+              //$middleware->add (route ('admin/', page ('platform/home.html')), null, 'notFound');
+            };
+          })
+        //
+        ->on (RECONFIGURE,
+          function (ApplicationRouterInterface $router) {
+            $router->add (Routes::class, 'platform');
+          });
   }
 
 }
