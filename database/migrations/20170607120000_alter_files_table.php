c660e84cf1f3b55faff59f51eb9b764cf7d42e78
Paulo Elias <paulo.elias@impactwave.com>
2017-06-07 12:17:40 +0100

    Criação de migração para alterar o campo owner_id na tabela files de modo a permitir NULL

---
+++ database/migrations/20170607120000_alter_files_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Electro\Plugins\IlluminateDatabase\AbstractMigration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AlterFilesTable extends AbstractMigration
+{
+  /**
+   * Reverse the migration.
+   *
+   * @return void
+   */
+  function down ()
+  {
+    $schema = $this->db->schema ();
+
+    if ($this->db->hasTable ('files')) 
+	{
+		$schema->table ('files', function (Blueprint $t) {
+			$table->integer('owner_id')->unsigned()->change();
+		});
+    }
+    else $this->output->writeln (" == Table <info>files</info> doesn't exist. Skipped.");
+  }
+
+  /**
+   * Run the migration.
+   *
+   * @return void
+   */
+  function up ()
+  {
+    $schema = $this->db->schema ();
+
+    if ($this->db->hasTable ('files'))
+	{
+		$schema->table ('files', function (Blueprint $t) {
+			$table->integer('owner_id')->nullable()->change();
+	    });
+	}
+	else $this->output->writeln (" == Table <info>files</info> doesn't exist. Skipped.");
+  }
+}
